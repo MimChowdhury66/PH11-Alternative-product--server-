d1d0f65e7ae1dd6348739e227f9eb2601571590e
mim chowdhury <umac3333@gmail.com>
2024-05-15 10:27:23 +0600

    update-worked

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express();
@@ -36,7 +36,51 @@ async function run() {
             const result = await queryCollection.insertOne(req.body);
             res.send(result)
         })
+        app.get('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const requestedQuery = await queryCollection.findOne(query);
+            res.send(requestedQuery)
+        })
+        app.put('/queries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+            const updateQuery = {
+                $set: {
+                    productBrand: query.productBrand,
+                    postedTimestamp: query.postedTimestamp,
+                    productImageURL: query.productImageURL,
+                    productName: query.productName,
+                    boycottingReasonDetails: query.boycottingReasonDetails,
+                    recommmendationCount: query.recommmendationCount,
+                    queryTitle: query.queryTitle,
+                    email: query.email,
+                    displayName: query.displayName,
+                    photoURL: query.photoURL
+                }
+            };
+            const result = await queryCollection.updateOne(filter, updateQuery, options);
+            res.send(result)
+        })
 
+
+        app.get('/queries', async (req, res) => {
+            let query = req.query;
+
+            // if (req.query?.email) {
+            //     query = { email: req.query.email }
+            // }
+
+            const result = await queryCollection
+                .find(query)
+                .sort({
+                    postedTimestamp: -1
+                })
+                .toArray();
+            res.send(result)
+        })
 
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
